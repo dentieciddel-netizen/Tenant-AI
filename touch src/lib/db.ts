2d19546ec3e61fedf1b1ebbb088c73161edb6d60
Dentie <dentieciddel@gmail.com>
2025-10-27 01:46:38 +0200

    Create db.ts

---
+++ touch src/lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof getPrismaClient> | undefined
+}
+
+function getPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? getPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
